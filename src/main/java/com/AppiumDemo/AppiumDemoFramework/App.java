87dd4835bf31374570a142fbe767408678022d28
adinath1212 <aadidhopte11@gmail.com>
2025-07-24 18:14:43 +0530

    all file added

---
+++ src/main/java/com/AppiumDemo/AppiumDemoFramework/App.java
@@ -0,0 +1,13 @@
+package com.AppiumDemo.AppiumDemoFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
